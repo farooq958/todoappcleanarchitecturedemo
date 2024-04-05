f0d611daa67a84932317ad0d4a232ab9ab8b6ce5
Farooq Khan <farooqafridi958@gmail.com>
2024-04-05 15:19:13 +0500

    extensions.dart

--- lib/SRC/Data/DataSource/Resources/Extensions/extensions.dart
+++ lib/SRC/Data/DataSource/Resources/Extensions/extensions.dart
@@ -8,6 +8,21 @@ extension CustomSizedBoxExt on num {
 
   SizedBox get y => SizedBox(height: toDouble());
 }
+extension ImageTypeExtension on String {
+  ImageType get imageType {
+    if (startsWith('http') || startsWith('https')) {
+      return ImageType.network;
+    } else if (endsWith('.svg')) {
+      return ImageType.svg;
+    } else if (startsWith('file://')) {
+      return ImageType.file;
+    } else {
+      return ImageType.png;
+    }
+  }
+}
+
+enum ImageType { svg, png, network, file, unknown }
 
 // extension OfficeTimingsExtension on String {
 //   List<String> getOfficeTimings(String season) {
